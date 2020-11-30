de317e49943323ec48d1c1d6dab0f10c72654ed3
mia-k-monaghan <emmykperdue@gmail.com>
2020-11-30 13:04:26 -0700

    updated user field in subscription model, registered model with admin

--- stripe/models.py
+++ stripe/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.conf import settings
 # Create your models here.
 
 class Subscription(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
     stripe_customer = models.CharField(max_length=100,
         help_text = "The user's Stripe Customer object, if it exists")
