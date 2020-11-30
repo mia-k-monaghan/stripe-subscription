75970514ef48ac4d53aac8123203e74f2973b40b
mia-k-monaghan <emmykperdue@gmail.com>
2020-11-30 12:54:33 -0700

    Created Subscription Model

--- stripe/models.py
+++ stripe/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class Subscription(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+    stripe_customer = models.CharField(max_length=100,
+        help_text = "The user's Stripe Customer object, if it exists")
+    stripe_subscription = models.CharField(max_length=100,
+        help_text = "The user's Stripe Customer object, if it exists")
+
+    def __str__(self):
+        return (self.email)
