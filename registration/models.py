de9bcd72bbc252ee336d769e1d4b950b967e4512
mia-k-monaghan <emmykperdue@gmail.com>
2020-11-30 11:55:00 -0700

    Ran User Setup Migrations

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class User(AbstractUser):
+    pass
