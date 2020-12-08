79f8213655e8306586b153ce445f816c0b3aa4ae
mia-k-monaghan <emmykperdue@gmail.com>
2020-12-08 11:42:58 -0700

    addded subscription view and url

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
 urlpatterns = [
-    
+    path('create-subscription', views.createSubscription, name='subscribe'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import JsonResponse
+import json
+import stripe
 
 stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
 # Create your views here.
+def createSubscription(request):
+    if request.method=='POST':
+        data = json.loads(request.data)
+        try:
+            # Attach the payment method to the customer
+            stripe.PaymentMethod.attach(
+                data['paymentMethodId'],
+                customer=data['customerId'],
+            )
+            # Set the default payment method on the customer
+            stripe.Customer.modify(
+                data['customerId'],
+                invoice_settings={
+                    'default_payment_method': data['paymentMethodId'],
+                },
+            )
+
+            # Create the subscription
+            subscription = stripe.Subscription.create(
+                customer=data['customerId'],
+                items=[
+                    {
+                        'price': 'price_HGd7M3DV3IMXkC'
+                    }
+                ],
+                expand=['latest_invoice.payment_intent'],
+            )
+            return JsonResponse(subscription)
+        except Exception as e:
+            return JsonResponse(error={'message': str(e)}), 200
