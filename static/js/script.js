c07834c459331992664f99a9e7f7d69c43048788
mia-k-monaghan <emmykperdue@gmail.com>
2020-12-08 11:16:34 -0700

    redesigned subscription flow

--- static/js/script.js
+++ static/js/script.js
@@ -1,63 +1,7 @@
-// If a fetch error occurs, log it to the console and show it in the UI.
-var handleFetchResult = function(result) {
-  if (!result.ok) {
-    return result.json().then(function(json) {
-      if (json.error && json.error.message) {
-        throw new Error(result.url + ' ' + result.status + ' ' + json.error.message);
-      }
-    }).catch(function(err) {
-      showErrorMessage(err);
-      throw err;
-    });
-  }
-  return result.json();
-};
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+let stripe = Stripe('pk_test_51HA03iLPfeVqJ0LGF7vJErtgow7hEF95tZc3jk1zhMmpXAcEfTR0mBSiPqu4oqlivxO9EAGfeIegQAIXzhUKbMWl00tdieedgt');
+let elements = stripe.elements();
 
-// Create a Checkout Session with the selected plan ID
-var createCheckoutSession = function(priceId) {
-  return fetch("/create-checkout-session", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      priceId: priceId
-    })
-  }).then(handleFetchResult);
-};
-
-// Handle any errors returned from Checkout
-var handleResult = function(result) {
-  if (result.error) {
-    showErrorMessage(result.error.message);
-  }
-};
-
-var showErrorMessage = function(message) {
-  var errorEl = document.getElementById("error-message")
-  errorEl.textContent = message;
-  errorEl.style.display = "block";
-};
-
-/* Get your Stripe publishable key to initialize Stripe.js */
-fetch("/setup")
-  .then(handleFetchResult)
-  .then(function(json) {
-    var publishableKey = json.publishableKey;
-    var basicPriceId = json.basicPrice;
-
-    var stripe = Stripe(publishableKey);
-    // Setup event handler to create a Checkout Session when button is clicked
-    document
-      .getElementById("basic-plan-btn")
-      .addEventListener("click", function(evt) {
-        createCheckoutSession(basicPriceId).then(function(data) {
-          // Call Stripe.js method to redirect to the new Checkout page
-          stripe
-            .redirectToCheckout({
-              sessionId: data.sessionId
-            })
-            .then(handleResult);
-        });
-      });
-  });
+let card = elements.create('card', { style: style });
+card.mount('#card-element');
