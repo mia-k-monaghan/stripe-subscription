eb32744bec81308835e6d033b0dde6d11901c0a1
mia-k-monaghan <emmykperdue@gmail.com>
2020-12-08 11:18:41 -0700

    added display error function

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,19 @@
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 let stripe = Stripe('pk_test_51HA03iLPfeVqJ0LGF7vJErtgow7hEF95tZc3jk1zhMmpXAcEfTR0mBSiPqu4oqlivxO9EAGfeIegQAIXzhUKbMWl00tdieedgt');
 let elements = stripe.elements();
-
 let card = elements.create('card', { style: style });
+
 card.mount('#card-element');
+card.on('change', function (event) {
+  displayError(event);
+});
+
+function displayError(event) {
+  changeLoadingStatePrices(false);
+  let displayError = document.getElementById('card-element-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+}
