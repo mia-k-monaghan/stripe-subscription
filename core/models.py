363285990696f2582129cd6de9b82f5f5f2f6caf
mia-k-monaghan <emmykperdue@gmail.com>
2020-12-10 12:49:58 -0700

    moved stripe_customer field to user model

--- core/models.py
+++ core/models.py
@@ -5,8 +5,6 @@ from django.conf import settings
 class Subscription(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
-    stripe_customer = models.CharField(max_length=100,blank=True,
-        help_text = "The user's Stripe Customer object, if it exists")
     stripe_subscription = models.CharField(max_length=100,blank=True,
         help_text = "The user's Stripe Customer object, if it exists")
 
