249d917b2213a524fdaac4e423b224c7034c5b22
mia-k-monaghan <emmykperdue@gmail.com>
2021-02-12 15:43:01 -0700

    added import/export model admins

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Subscription
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
 # Register your models here.
 
-admin.site.register(Subscription)
+class SubscriptionResource(resources.ModelResource):
+    class Meta:
+        model=Subscription
+        fields=('user','active','tracking')
+
+class SubscriptionAdmin(ImportExportModelAdmin):
+    resource_class = SubscriptionResource
+
+admin.site.register(Subscription, SubscriptionAdmin)
