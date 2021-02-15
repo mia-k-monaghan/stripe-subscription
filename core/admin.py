0cecdac06aea19aaef311d54186a8cdf5955fff6
mia-k-monaghan <emmykperdue@gmail.com>
2021-02-15 15:47:16 -0700

    added fulfillment field to subscription model

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Subscription
+from .models import Subscription,MonthlyOrder
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+
+import tablib
 
 
 # Register your models here.
@@ -9,9 +11,21 @@ from import_export.admin import ImportExportModelAdmin
 class SubscriptionResource(resources.ModelResource):
     class Meta:
         model=Subscription
-        fields=('user','active','tracking')
+        fields=['id','user__email','active','fulfilled']
 
 class SubscriptionAdmin(ImportExportModelAdmin):
     resource_class = SubscriptionResource
+    list_display = ['user','fulfilled','active']
+    list_filter = ['active','fulfilled']
+
+class MonthlyOrderResource(resources.ModelResource):
+    class Meta:
+        model=MonthlyOrder
+        fields=['subscription','tracking']
+
+class MonthlyOrderAdmin(ImportExportModelAdmin):
+    resource_class = MonthlyOrderResource
+
 
 admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(MonthlyOrder, MonthlyOrderAdmin)
--- stripe_subscription/settings.py
+++ stripe_subscription/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'core',
 ]
 
+IMPORT_EXPORT_USE_TRANSACTIONS = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
